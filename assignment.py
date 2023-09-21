a4f2cbc374f9f870b12b82b7f106113ee0d6d8ac
alliajagbe <alliajagbe001@gmail.com>
2023-09-21 22:06:40 +0530

    three estimators

---
+++ assignment.py
@@ -0,0 +1,17 @@
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+#%%
+'''
+Question 1
+'''
+n = 5000
+x = np.random.normal(0, 1, n)
+divisor = 1/(n - 1)
+capital_S = np.sqrt(divisor * np.sum((x - np.mean(x))**2))
+mean = np.mean(abs(x - np.mean(x)))
+# discarding the smallest 0.1n/2 and largest 0.1n/2
+new_x = np.sort(x)
+new_x = new_x[int(0.1*n/2):int(n - 0.1*n/2)]
+trimmed_S = np.sqrt(divisor * np.sum((new_x - np.mean(new_x))**2))
