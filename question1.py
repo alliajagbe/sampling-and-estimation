f0e434bafa9b5b5317cb470f6c8e33b36aab023b
alliajagbe <alliajagbe001@gmail.com>
2023-09-28 05:32:07 +0530

    spacing

--- question1.py
+++ question1.py
@@ -135,6 +135,7 @@ def mse(estimator, true_value):
     return np.var(estimator) + (np.mean(estimator) - true_value)**2
 
 # for normal
+print("Normal Distribution")
 capital_Ss, means, trimmed_Ss = normal(n)
 print("Bias of Capital S:", bias(capital_Ss, 1))
 print("Bias of Mean Deviations:", bias(means, 1))
@@ -143,7 +144,9 @@ print("MSE of Capital S:", mse(capital_Ss, 1))
 print("MSE of Mean Deviations:", mse(means, 1))
 print("MSE of Trimmed S:", mse(trimmed_Ss, 1))
 print()
+
 # for mixture of normals
+print("Mixture of Two Normals")
 capital_Ss, means, trimmed_Ss = mixture_of_normals(1, 3, n)
 print("Bias of Capital S:", bias(capital_Ss, 1))
 print("Bias of Mean Deviations:", bias(means, 1))
@@ -152,8 +155,10 @@ print("MSE of Capital S:", mse(capital_Ss, 1))
 print("MSE of Mean Deviations:", mse(means, 1))
 print("MSE of Trimmed S:", mse(trimmed_Ss, 1))
 print()
+
 # for normal plus cauchy
-capital_Ss, means, trimmed_Ss = normal_plus_cauchy(1, 1, n)
+print("Normal Plus Cauchy")
+capital_Ss, means, trimmed_Ss = normal_plus_cauchy(1, n)
 print("Bias of Capital S:", bias(capital_Ss, 1))
 print("Bias of Mean Deviations:", bias(means, 1))
 print("Bias of Trimmed S:", bias(trimmed_Ss, 1))
